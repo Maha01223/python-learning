587e715b53e3f52a0e4324838a3c1415ce1b6f39
MOhsin <MOhsin.gani78692@gmail.com>
2026-02-02 17:41:01 +0530

    add

---
+++ basic/practice.py
@@ -0,0 +1,19 @@
+"""Dictionaries: - dict are unorder and changeble and do not alow dublicate
+                dict is a used to store data valves in key: valve pair
+                python 3.7 dicts in order
+                python 3.6 dicts in unordered
+                It is writen {}"""
+
+"""#Creating Dict
+empty_dict={}
+print(type(empty_dict))
+
+#OR
+
+empty_dicts = dict()
+print(type(empty_dicts)) """   
+
+
+student = {"name": "Mohsin", "age": 34, "grade": "A"}
+print(student)
+print(type(student))
