c01964b42883c00ec7a89e2440664d497bef4fc9
MOhsin <MOhsin.gani78692@gmail.com>
2026-02-05 17:37:04 +0530

    add

--- data_structure/list.py
+++ data_structure/list.py
@@ -24,7 +24,77 @@ print(fruits[4])"""
 
 
 #find all fruis
-print(fruits[0:])
+"""print(fruits[0:])
 print(fruits[1:])
 print(fruits[-1:])
-print(fruits[:2])
+print(fruits[:2])"""
+
+
+#medifying the list elemens
+
+"""print(fruits)
+fruits[1]= 'watermelon'
+print(fruits)
+
+fruits[1:]= 'watermelon'
+print(fruits)"""
+
+#List Methods
+
+"""fruits.append("mango")
+print(fruits)
+
+fruits.insert(6,'pineapple')
+print(fruits)
+fruits.extend([101,102,103,104,105,106,107])
+print(fruits)"""
+
+
+#Remove
+"""fruits.pop() #remove last item on the list
+print(fruits)
+
+fruits.pop(0) #remove zero index item on the list
+print(fruits)
+
+fruits.remove(101) #remove a perticular item on the list
+print(fruits)
+
+#fruits.clear() #remove all the items on the list
+print(fruits)
+
+new_list= fruits.pop(4)
+print(new_list)"""
+
+
+Branch = ['a','b','c','d','e','f','g','a']
+
+"""print('a' in Branch)
+print(Branch.count('a'))
+
+
+#print(Branch)
+#Branch.sort() #sort means the iem of the list change our original place
+print(Branch)
+
+new_banch = Branch[:]
+print(new_banch)
+
+#new_banch.sort()
+print(new_banch)
+#sort or sorted both are same
+print(sorted(Branch))
+
+Branch.reverse()
+print(Branch)
+
+Branch [:] = Branch.copy() #both are right to copy
+print(len(Branch))"""
+
+sentence = ' '
+
+new_sentence = sentence.join(['Hello']) #
+#new_sentence = ' '.join([Hello])  boh are same working
+
+print(new_sentence)
+
