1db4a9e3721bb02fb94ba1553a781d67998e5eaa
MOhsin <MOhsin.gani78692@gmail.com>
2026-02-05 15:35:19 +0530

    dict

---
+++ data_structure/list.py
@@ -0,0 +1,30 @@
+#data structure
+#List are ordered, changable allow duplictae member and  mutable colection of items they can contain item of different data types 
+
+#empy list
+"""lst = []
+print(type(lst))
+
+names = ["Mohsin", "jack", "jacob", 1,2,3,4]
+print(names)
+
+mixes = ["mohsin", 1, 3.14, True]
+print(mixes)"""
+
+
+#Accessing List elements
+
+fruits = ["apple", "banana", "cherry", "kiwi", "gauva"]
+
+"""#find fruits index
+print(fruits[0])
+print(fruits[2])
+print(fruits[3])
+print(fruits[4])"""
+
+
+#find all fruis
+print(fruits[0:])
+print(fruits[1:])
+print(fruits[-1:])
+print(fruits[:2])
