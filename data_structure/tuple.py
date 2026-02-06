a2f7c96f26ca35dcc6d7e333661eeef726011f1c
MOhsin <MOhsin.gani78692@gmail.com>
2026-02-06 16:23:44 +0530

    add

---
+++ data_structure/tuple.py
@@ -0,0 +1,19 @@
+#Tuple
+#tupe are order colection of iems tha are immuable they are similar to list,bu their immuable make hem different
+
+#tuple are used o sore mutiple iem in a single variable and its a collection which is ordered and unchange.
+
+#creating empty tuple
+
+"""empty_tuple = ()
+print(type(empty_tuple))
+
+lst = list()
+print(type(lst))
+
+tuple = tuple()
+print(type(tuple))"""
+
+mix = (3.14, True, "hello", 10)
+print(type(mix))
+print(mix)
