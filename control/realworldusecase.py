9b43b4eb593fd4f00de33ad49f1b119886248290
MOhsin <MOhsin.gani78692@gmail.com>
2026-02-09 00:18:01 +0530

    add

--- control/realworldusecase.py
+++ control/realworldusecase.py
@@ -27,7 +27,7 @@ for task in to_do_list:
 # Organizing students Grades
 #Create a list to store and calculate average grade for students.
 
-grade = [86,92,28,90,88]
+"""grade = [86,92,28,90,88]
 
 #add
 grade.append(95)
@@ -42,5 +42,55 @@ print(f"Average Grade: {Average_Grade: .2f}")
 higest_grade = max(grade)
 print(f"higest Grade: {higest_grade}")
 lowest_grade = min(grade)
-print(f"lowest  Grade: {lowest_grade}")
+print(f"lowest  Grade: {lowest_grade}")"""
 
+
+#Managing An Inventory
+#use a list to manage inventory iems in a store.    
+
+"""invenory = ["apple","banana","orange","grapes"]
+
+#adding a new items
+invenory.append('strawberies')
+print(invenory)
+
+#removing itesm  that iss out of stock.
+invenory.remove('banana')
+print(invenory)
+
+#checking if an item is in stock
+
+item = "orange"
+
+if item in invenory:
+    print(f"{item} are in stock")
+else:
+    print(f"{item} are out of stock")
+
+
+#printing the inventory
+
+print("Inventory list:")
+for item in invenory:
+    print(f"-{item}")"""
+
+
+#Collecting User Feedback
+
+# use a list to collect and analyze user feedback
+
+feedback= ["grae service","very satisfied","coud be better","excelent experience"]
+
+#adding
+feedback.append("Not happy with the services")
+
+#counting specific feedback
+posiive_feedback = sum (1 for comment in feedback if "greet" in comment.lower() or "excelent" in comment.lower())
+
+print(f" feedback count: {posiive_feedback}")
+
+#prining all feedback
+
+print("user feedback")
+for comment in feedback:
+    print(f"-{comment}")
