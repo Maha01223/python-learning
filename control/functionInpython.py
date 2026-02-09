106ec2b7ef51fc64a15bcbe520f2385f6d1ea8da
MOhsin <MOhsin.gani78692@gmail.com>
2026-02-09 15:25:02 +0530

    add

---
+++ control/functionInpython.py
@@ -0,0 +1,29 @@
+#A function is a block of code that perform a specific ask.
+#Function help in Organizing code. Reusing code and Improving Readatrility.
+
+
+#syntax
+def function_name(parameters):
+    """docstring"""
+    #function body
+    return Exception
+
+#why function??
+num = 24
+if num%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+
+#In Function
+
+def even_or_odd(num):
+    """this function find even or odd"""
+    if num%2==0:
+        print("the number is even")
+    else:
+        print("the number is odd")
+
+#call this function.
+even_or_odd(21)
