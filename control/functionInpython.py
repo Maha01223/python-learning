5668b21b1e509ae84eb02b2a5f02d2ab64f73dbd
MOhsin <MOhsin.gani78692@gmail.com>
2026-03-01 01:48:03 +0530

    add

--- control/functionInpython.py
+++ control/functionInpython.py
@@ -117,15 +117,18 @@ print(convert_temperature(1, "C"))
 
 def is_strong_password(password):
     """this is strong password  if password is strong or not"""
-    if len(password) > 8:
+    if len(password) < 8:
         return False
-    if not any (char.isdigital() for char in password):
+    if not any(char.isdigit() for char in password):
         return False
-    if not any (char.islower() for char in password):
+    if not any(char.islower() for char in password):
         return False
-    if not any (char in '!@#$%^&*+*/-' for char in password):
+    if not any(char.isupper() for char in password):
+        return False
+    if not any(char in '!@#$%^&*+*/-' for char in password):
         return False
     return True
 
 
-print(is_strong_password(weekend@123))
+print(is_strong_password("weekend@123"))
+print(is_strong_password("Str0ngPWd1123!)"))
