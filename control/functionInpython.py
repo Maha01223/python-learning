66c24c6f3405c28e6ad514f55080dd27e158f5f4
MOhsin <MOhsin.gani78692@gmail.com>
2026-03-11 11:11:44 +0530

    add

--- control/functionInpython.py
+++ control/functionInpython.py
@@ -134,5 +134,22 @@ def is_strong_password(password):
 print(is_strong_password("Str0ngPWd1123!)"))
 print(is_strong_password("StT0ngPWd1123!)"))"""
 
-#hghgs
-#jhg
+#3. calculate the total cos of iems in a shoppingcard
+def calculate_total_cost(cart):
+    total_cost=0
+    for item in cart:
+        total_cost+= item['price']* item['quantity']
+    return total_cost
+
+#exampe cart data
+
+cart = [
+    {'name': 'apple', 'price': 0.5, 'quantity': 4},
+    {'name': 'orange', 'price': 0.7, 'quantity': 5},
+    {'name': "banana", 'price': 0.3, 'quantity': 10}
+]
+
+#caling the function
+
+total_cost = calculate_total_cost(cart)
+print(total_cost)
