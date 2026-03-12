63c779edb51365eb8bbe10d445acd00f4ab806a8
MOhsin <MOhsin.gani78692@gmail.com>
2026-03-12 16:21:44 +0530

    add

---
+++ modules/package/subpackages/mul.py
@@ -0,0 +1,5 @@
+def multiple(a,b):
+    return a*b
+
+def divided(a,b):
+    return a/b
---
+++ modules/test.py
@@ -0,0 +1,9 @@
+from package.maths import addition
+print(addition(62,54))
+
+
+from package.maths import substruction
+print(substruction(6,85))
+
+from package import maths
+print(maths.substruction(65,54))
